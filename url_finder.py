b2427370b1b997b5cc9be3e3eb2cb254a2215d4d
justin-nevermore <justin.jagdeep.ar@gmail.com>
2020-07-20 15:39:13 +0530

    url key updated in window tracker.json

--- url_finder.py
+++ url_finder.py
@@ -1,42 +1,49 @@
-
 import browserhistory as bh
 import json
+from common import unicode_convert
 
-history_data = bh.get_browserhistory()  # brings chrom & firbox  browser history
-
-chrome_dict = {}
-with open("window_tracker.json", 'r') as F:
-    d = json.loads(F.read())
-for key1, value1 in d.items():  # from recorded window removing non chrome windows
-    json_window_name = value1['window']
-    if 'chrome' in json_window_name.lower():
-        chrome_dict[key1] = value1
-    else:
-        pass
+def history():
+    return bh.get_browserhistory()  # brings chrom & firbox  browser history
 
 
 def get_url():
-    final_url = []
+    chrome_dict = {}
+    with open("window_tracker.json", 'r') as F:
+        d = json.loads(F.read())
+    for key1, value1 in d.items():  # from recorded window removing non chrome windows
+        json_window_name = value1['window']
+        if 'chrome' in json_window_name.lower():
+            chrome_dict[key1] = value1
+        else:
+            pass
+    pop_key = []
+    history_data = history()
     for key2, value2 in history_data.items():  # from history tracker getting all history from chrome only
-        if key2.lower() == 'chrome':  # to get only from chrome
-            for key3, value3 in chrome_dict.items():  # checking in sorted chrome dict
-                json_page_name = value3['window'].replace(" - Google Chrome", "")
-                for items in value2:
-                    url = items[0]
-                    url_page_name = items[1]
-                    url_time = items[2]
-                    if json_page_name.lower() == url_page_name.lower():
-                        if final_url:
-                            for it in final_url:
-                                if it != url:
-                                    final_url.append(url)
-                                else:
-                                    pass
-                        else:
-                            final_url.append(url)
+        if key2.lower() == 'chrome':
+            for items in value2:
+                url = items[0]
+                url_page_name = items[1]
+                url_time = items[2]
+                for key3, value3 in chrome_dict.items():
+                    hif_data = unicode_convert(value3['window'])
+                    json_page_name = value3['window'].split(hif_data)[0]
+                    if url_page_name.lower() in json_page_name.lower():
+                        pop_key.append(key3)
+                        chrome_dict[key3]['url'] = str(url)
+                        break
                     else:
                         pass
         else:
             pass
-    return final_url
+
+    for item in pop_key:
+        d.pop(item)
+    d.update(chrome_dict)
+    with open("url_updated.json",'w+') as f:
+        f.write(json.dumps(d))
+    return d
+
+
+get_url()
+
 
